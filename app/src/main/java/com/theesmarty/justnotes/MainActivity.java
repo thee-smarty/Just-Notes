fc4ac3c625561ff830bf65f6414187e84ab6ebb2
The-Smarty <phanipraharsha25@gmail.com>
2024-05-23 10:12:35 +0530

    Added functionality to button add

--- app/src/main/java/com/theesmarty/justnotes/MainActivity.java
+++ app/src/main/java/com/theesmarty/justnotes/MainActivity.java
@@ -2,9 +2,11 @@ package com.theesmarty.justnotes;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,10 +15,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -24,10 +24,16 @@ import com.google.firebase.auth.GoogleAuthProvider;
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private GoogleSignInClient googleSignInClient;
+    Button add;
+    ListView list;
+    TextView info;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        add = findViewById(R.id.add);
+        list = findViewById(R.id.list);
+        info = findViewById(R.id.info);
 
         GoogleSignInOptions gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken("829993235030-j3negle747muq1jcc40rpn8of65bt9vv.apps.googleusercontent.com")
@@ -43,7 +49,13 @@ public class MainActivity extends AppCompatActivity {
         }
         else {
             Toast.makeText(getApplicationContext(), "Login Success!\nWelcome "+user.getDisplayName(), Toast.LENGTH_SHORT).show();
+            add.setEnabled(true);
         }
+
+        add.setOnClickListener(view -> {
+            Intent in = new Intent(MainActivity.this, NoteActivity.class);
+            startActivity(in);
+        });
         //Add the functionality of the app from here
 
     }
@@ -58,15 +70,12 @@ public class MainActivity extends AppCompatActivity {
                 GoogleSignInAccount googleSignInAccount = signInAccountTask.getResult(ApiException.class);
                 if (googleSignInAccount != null) {
                     AuthCredential authCredential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(), null);
-                    firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (signInAccountTask.isSuccessful()) {
-                                Toast.makeText(getApplicationContext(), "Login Success!", Toast.LENGTH_SHORT).show();
-                            } else {
-                                // Log the error or display a more informative error message
-                                Toast.makeText(getApplicationContext(), "Error Login!", Toast.LENGTH_SHORT).show();
-                            }
+                    firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(this, task -> {
+                        if (signInAccountTask.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Login Success!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Log the error or display a more informative error message
+                            Toast.makeText(getApplicationContext(), "Error Login!", Toast.LENGTH_SHORT).show();
                         }
                     });
                 }

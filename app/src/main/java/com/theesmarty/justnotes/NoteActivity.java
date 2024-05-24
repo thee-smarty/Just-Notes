97cdc094c481ff4d94a89cf11f28903b586e3fb5
The-Smarty <phanipraharsha25@gmail.com>
2024-05-24 17:59:39 +0530

    Added elements to NoteActivity

--- app/src/main/java/com/theesmarty/justnotes/NoteActivity.java
+++ app/src/main/java/com/theesmarty/justnotes/NoteActivity.java
@@ -1,17 +1,20 @@
 package com.theesmarty.justnotes;
 
-// use sql for online the data
 
+import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class NoteActivity extends AppCompatActivity {
-
+    EditText title,content;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+        title = findViewById(R.id.title);
+        content = findViewById(R.id.content);
+
 
     }
 }
